c597a5bd7bd275034b45a16bceae5e8a9fcea99f
Viacheslav <wooofah@gmail.com>
2020-03-22 15:17:17 +0200

    added form and connect with App state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from './components/Navbar';
+import { TodoForm } from './components/TodoForm';
 
 const App: React.FC = () => {
+
+  const [todos, setTodos] = useState([])
+
+  const addHandler = (title: string) => {
+    console.log(title)
+  }
   return (
     <>
       <Navbar />
       <div className="container">
-        <h1>Test</h1>
+        <TodoForm onAdd={addHandler} />
       </div>
-      </>
+    </>
   );
 }
 
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useRef } from 'react'
+
+interface TodoFormProps {
+  onAdd(title: string): void
+}
+
+export const TodoForm: React.FC<TodoFormProps> = (props) => {
+  // const [title, setTitle] = useState<string>('')
+  const ref = useRef<HTMLInputElement>(null)
+
+  // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setTitle(event.target.value)
+  // }
+
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      props.onAdd(ref.current!.value);
+      ref.current!.value = ''
+    }
+  }
+
+  return (
+    <div className="input-field mt2">
+      <input
+        // value={title}
+        ref={ref}
+        type="text"
+        id="title"
+        placeholder="Enter your Todo.."
+        // onChange={changeHandler}
+        onKeyPress={keyPressHandler}
+      />
+      <label htmlFor="title" className="active">
+        Enter your Todo
+      </label>
+    </div>
+  )
+}
